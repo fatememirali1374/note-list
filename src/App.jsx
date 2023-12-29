38917ecde468a794dcf5308d1536aa0c05b2903f
fateme Mirali <fatememirali1374@gmail.com>
2023-12-29 22:18:40 +0330

    create new not form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import "./App.css";
+import AddNewNote from "./components/AddNewNote";
 function App() {
-  return <h1>02: NoteApp starter file</h1>;
+  return(
+    <div className="container">
+      <div className="note-header">
+        <h1>My Notes (0) </h1>
+        <div>sorting</div>
+      </div>
+      <div className="note-app">
+          <AddNewNote/>
+        
+        <div className="note-container">notes</div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
+ 
