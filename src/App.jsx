f8e364485fe19356143933e7d2a5f9991ac26339
fateme Mirali <fatememirali1374@gmail.com>
2024-01-20 14:01:35 +0330

    changed useState to useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,55 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import "./App.css";
 import AddNewNote from "./components/AddNewNote";
 import NoteList from "./components/NoteList";
 import NoteStatus from "./components/NoteStatus";
 import NoteHeader from "./components/NoteHeader";
+
+const notesReducer=(notes,{type, payload})=>{
+ switch(type){
+  case "add":{
+    return [...notes, payload]
+  }
+  case "delete":{
+    return notes.filter((n)=>n.id!==payload)
+  }
+  case "complete":{
+    return notes.map((note)=>
+     note.id===payload?{...note,completed:!note.completed}:note)
+    
+  }
+  default:
+    throw new Error("unknown Error"+ type)
+ }
+}
+
 function App() {
-  const [notes, setNotes]= useState([])
+  // const [notes, setNotes]= useState([])
+  const[notes, dispatch]= useReducer(notesReducer,[])
   const [sortBy, setSortBy]=useState("latest")
   const handleAddNote=(newNote)=>{
-    setNotes(prevNotes=>[...prevNotes, newNote])
+    // setNotes(prevNotes=>[...prevNotes, newNote])
+    dispatch({type:"add", payload:newNote})
   }
   const handleDeleteNote=(id)=>{
     // const filteredNotes= notes.filter((n)=>n.id!==id)
     // setNotes(filteredNotes)
-    setNotes(prevNotes=>prevNotes.filter((n)=>n.id!==id))
+    // or
+    // setNotes(prevNotes=>prevNotes.filter((n)=>n.id!==id))
+dispatch({type:"delete", payload:id})
   }
   const handleCompleteNote=(e)=>{
 const noteId=Number(e.target.value)
 // const newNotes=notes.map((note)=>
 //   note.id===noteId?{...note,completed:!note.completed}:note
 // )
-setNotes((prevNotes)=>
-prevNotes.map((note)=>
- note.id===noteId?{...note,completed:!note.completed}:note)
-)}
+// or
+// setNotes((prevNotes)=>
+// prevNotes.map((note)=>
+//  note.id===noteId?{...note,completed:!note.completed}:note)
+// )
+dispatch({type:"complete",payload:noteId})
+}
 
  return(
     <div className="container">
